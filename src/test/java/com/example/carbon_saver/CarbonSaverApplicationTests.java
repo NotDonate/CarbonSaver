fd7f2ef36910e870684bb6f424c3ee89dd52c4af
Dontae Clarke <DontaeClarke@outlook.com>
2026-03-08 20:24:43 +0000

    Add Spring Boot backend implementation

---
+++ src/test/java/com/example/carbon_saver/CarbonSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carbon_saver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
